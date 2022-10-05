782a554985a2179493be8bedc52bdd3a65e541a0
IbrahimGanaMalgwi <108299271+IbrahimMalgwi@users.noreply.github.com>
2022-10-05 05:56:36 +0100

    Update to codes

--- src/brickBreaker/GamePlay.java
+++ src/brickBreaker/GamePlay.java
@@ -1,2 +1,94 @@
-package brickBreaker;public class GamePlay {
+package brickBreaker;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
+    private boolean play = false;
+    private int score = 0;
+    private int totalBreaks = 21;
+
+    private Timer timer;
+    private int delay = 8;
+
+    private int playerX = 310;
+
+    private int ballposX = 120;
+    private int ballposY = 350;
+
+    private int ballxdir = -1;
+    private int ballydir = -2;
+
+    public GamePlay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.black);
+        g.fillRect(1,1, 692, 592);
+
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        g.setColor(Color.yellow);
+        g.fillRect(playerX,  ballposY, 20, 20);
+
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            if (playerX >= 600){
+                playerX = 600;
+            } else {
+                moveRight();
+            }
+
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT){
+            if (playerX < 10){
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+        }
+    }
+
+    private void moveLeft() {
+        play = true;
+        playerX = 20;
+    }
+
+    private void moveRight() {
+        play = true;
+        playerX -= 20;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
 }
--- src/brickBreaker/Main.java
+++ src/brickBreaker/Main.java
@@ -1,2 +1,16 @@
-package brickBreaker;public class Main {
+package brickBreaker;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame gameBoard = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        gameBoard.setBounds(10, 10, 700, 600);
+        gameBoard.setTitle("Breakout Ball");
+        gameBoard.setResizable(false);
+        gameBoard.setVisible(true);
+        gameBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameBoard.add(gamePlay);
+    }
 }
